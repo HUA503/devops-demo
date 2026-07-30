64bacc191a0846990d579299d8fcc1f52bb73709
zzc <zzc@example.com>
2026-07-30 01:50:07 +0000

    fix: javax.annotation.Resource -> @Autowired for Java 17

--- src/main/java/com/demo/controller/MessageController.java
+++ src/main/java/com/demo/controller/MessageController.java
@@ -4,7 +4,7 @@ import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
-import javax.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 public class MessageController {
@@ -12,7 +12,7 @@ public class MessageController {
     private static final Logger log =
         LoggerFactory.getLogger(MessageController.class);
 
-    @Resource
+    @Autowired
     private RocketMQTemplate rocketMQTemplate;
 
     @GetMapping("/api/send")
